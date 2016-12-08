8aa0e0253b1114b78b968a9c2eddc4bc9f97201b
Gabriel Guarisa <gabrielguarisa@gmail.com>
2016-12-08 15:46:20 -0200

    Screenshot to cpp file

--- 3DViewer/Screenshot.cpp
+++ 3DViewer/Screenshot.cpp
@@ -15,10 +15,10 @@ void ogl::Screenshot::newPhoto(ImageFormat imageFormat)
 	//get current viewport
 	glGetIntegerv(GL_VIEWPORT, this->viewport_);
 
-	this->weidth_ = this->viewport_[2];
+	this->width_ = this->viewport_[2];
 	this->height_ = this->viewport_[3];
 
-	this->bits_ = new GLubyte[this->weidth_ * 3 * this->height_];
+	this->bits_ = new GLubyte[this->width_ * 3 * this->height_];
 
 	//read pixel from frame buffer
 	glFinish(); //finish all commands of OpenGL
@@ -26,16 +26,16 @@ void ogl::Screenshot::newPhoto(ImageFormat imageFormat)
 	glPixelStorei(GL_PACK_ROW_LENGTH, 0);
 	glPixelStorei(GL_PACK_SKIP_ROWS, 0);
 	glPixelStorei(GL_PACK_SKIP_PIXELS, 0);
-	glReadPixels(0, 0, this->weidth_, this->height_, GL_BGR_EXT, GL_UNSIGNED_BYTE, this->bits_);
+	glReadPixels(0, 0, this->width_, this->height_, GL_BGR_EXT, GL_UNSIGNED_BYTE, this->bits_);
 
-	capImg_ = cvCreateImage(cvSize(this->weidth_, this->height_), IPL_DEPTH_8U, 3);
+	capImg_ = cvCreateImage(cvSize(this->width_, this->height_), IPL_DEPTH_8U, 3);
 	for (int i = 0; i < this->height_; ++i)
 	{
-		for (int j = 0; j < this->weidth_; ++j)
+		for (int j = 0; j < this->width_; ++j)
 		{
-			capImg_->imageData[i*capImg_->widthStep + j * 3 + 0] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->weidth_ + j * 3 + 0]);
-			capImg_->imageData[i*capImg_->widthStep + j * 3 + 1] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->weidth_ + j * 3 + 1]);
-			capImg_->imageData[i*capImg_->widthStep + j * 3 + 2] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->weidth_ + j * 3 + 2]);
+			capImg_->imageData[i*capImg_->widthStep + j * 3 + 0] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->width_ + j * 3 + 0]);
+			capImg_->imageData[i*capImg_->widthStep + j * 3 + 1] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->width_ + j * 3 + 1]);
+			capImg_->imageData[i*capImg_->widthStep + j * 3 + 2] = (unsigned char)(this->bits_[(this->height_ - i - 1) * 3 * this->width_ + j * 3 + 2]);
 		}
 	}
 
